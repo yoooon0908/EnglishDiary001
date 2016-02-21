95359b8f42c911fd3e44420282d51a2922dca621
三浦宏予 <yoooon0908@yahoo.co.jp>
2016-02-21 09:15:02 +0800

    start commit

---
+++ EnglishDiary/FourthViewController.swift
@@ -0,0 +1,107 @@
+//
+//  FourthViewController.swift
+//  EnglishDiary
+//
+//  Created by 三浦宏予 on 2016/02/20.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+import UIKit
+
+class FourthViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    
+    @IBOutlet weak var foTableView: UITableView!
+    //気分をリストで表示できるようにする
+    //辞書＋辞書
+    var englist:[NSDictionary] = []
+    var select = -1
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //行数
+    func tableView(tabeleView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return englist.count
+    }
+    
+    //表示するセルの中身2
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell{
+        var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+        
+        
+        //cell.textLabel!.text = "d1"
+        
+        
+        //        cell.textLabel!.text = "\(indexPath.row)行目"
+        cell.textLabel!.text = "\(englist[indexPath.row]["En"])"
+        //        //文字を茶色にする
+        //        cell.textLabel!.textColor = UIColor.brownColor()
+        //        //チェックマークをつける
+        //        cell.accessoryType = .DisclosureIndicator
+        //        cell.textLabel!.font = UIFont.systemFontOfSize(20)
+        
+        return cell
+    }
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        //json.txtファイルを読み込んで
+        var path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+        var jsondata = NSData(contentsOfFile: path!)
+        
+        
+        let jsonDictionaray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSArray
+        
+        
+        for data in jsonDictionaray {
+            var d1 = data["En"] as! String
+            var d2 = data["Ja"] as! String
+            //var d1 = data
+            
+            print(d1)
+            self.englist.append(data as! NSDictionary)
+            
+        }
+        
+    }
+    
+    // 選択された時に行う処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("\(indexPath.row)行目を選択")
+        select = indexPath.row
+        performSegueWithIdentifier("showFifthView",sender: nil)
+    }
+
+    // Segueで画面遷移する時
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var FifthVC = segue.destinationViewController as! FifthViewController
+        
+        FifthVC.fifSelect = select
+    
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ EnglishDiary/SecondViewController.swift
@@ -0,0 +1,86 @@
+//
+//  SecondViewController.swift
+//  EnglishDiary
+//
+//  Created by 三浦宏予 on 2016/02/20.
+//  Copyright © 2016年 Hiroyo Miura. All rights reserved.
+//
+import UIKit
+
+class SecondViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    var secSelect = -1
+    var englist:[NSDictionary] = []
+    
+    @IBOutlet weak var dateText001: UITextField!
+    @IBOutlet weak var titleText001: UITextField!
+    @IBOutlet weak var secImageView: UIImageView!
+    @IBOutlet weak var secTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //★の内容をtextfildに表示させる
+    
+    @IBAction func tapStar(sender: UIButton) {
+        secTextView.text = "リストからデータをもってくる"
+    }
+    
+    
+    @IBAction func tapPhoto(sender: UIButton) {
+        
+        // フォトライブラリが使用可能か？
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            
+            // フォトライブラリの選択画面を表示
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    // 写真選択時に呼ばれる
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        // 選択した画像を取得
+        if info[UIImagePickerControllerOriginalImage] != nil {
+            if let photo:UIImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                
+                // ImageViewにその画像を設定
+                secImageView.image = photo
+            }
+        }
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
+
+    
+    override func viewWillAppear(animated: Bool) {
+        //jsonファイル読み込む
+        let path = NSBundle.mainBundle().pathForResource("json", ofType: "txt")
+        let jsondata = NSData(contentsOfFile: path!)
+        //辞書データに変換
+        let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSArray
+        
+        let dic = jsonArray[secSelect]
+        
+       
+        secTextView.text = dic["En"] as! String
+    }
+    
+
+    
+      
+}
+
